8444266b91e1d462f93b45db7ec5f38c1f3b5ee7
leonlevy <leonlevy@machobike.local>
2019-11-28 12:32:27 -0800

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+sudo apt-get update
+sudo apt-get install unzip
+
+# Install terraform
+wget https://releases.hashicorp.com/terraform/0.12.16/terraform_0.12.16_linux_amd64.zip
+unzip terraform_0.12.16_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+terraform --version
+
+# Install AWS CLI
+curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+sudo ln -s /usr/local/bin/aws2 /usr/local/bin/aws
+mkdir .aws
+cp /vagrant/config .aws/
+cp /vagrant/credentials .aws/
+
+# Clone ec2 terraform project
+git clone https://github.com/bugbiteme/ec2-terraform.git
+
+
+
+# Cleanup
+rm -f *.zip
+rm -rf aws
